bb6a4c31fd737a7807d9c4f209de4604170f8dc2
asma ezzati <asmaezzati2019@gmail.com>
2025-10-05 15:21:35 +0330

    add colors & fonts with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      red: "#6F1D1B",
+      lion: "#BB9457",
+      bistre: "#432818",
+      brown: "#99582A",
+      peach: "#FFE6A7",
+    },
+    fontFamily: {
+      Vazir: ["Vazir"],
+    },
   },
   plugins: [],
 };
