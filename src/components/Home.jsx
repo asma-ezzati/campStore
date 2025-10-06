e4707951d13143eeb0da96b6fd95471c4f0658bd
asma ezzati <asmaezzati2019@gmail.com>
2025-10-06 13:09:42 +0330

    add background image in home components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,17 @@
+import banner from "../assets/banner1.webp";
+
 const Home = () => {
   return (
     <>
-      <h1>Home</h1>
+      {/* <img src={banner}></img> */}
+      <div
+        className="h-screen w-full bg-cover bg-center bg-no-repeat bg-fixed"
+        style={{ backgroundImage: `url(${banner})` }}
+      >
+        <div className="flex items-center justify-center h-full bg-black/50">
+          <h1 className="text-white text-4xl font-bold">Welcome to My Site</h1>
+        </div>
+      </div>
     </>
   );
 };
