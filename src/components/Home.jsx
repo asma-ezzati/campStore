61012990c9df7265648a6d3d879405a7359e9b4d
asma ezzati <asmaezzati2019@gmail.com>
2025-10-27 19:00:49 +0330

    style: add h1 in home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,21 @@
+import { Link } from "react-router-dom";
 import banner from "../assets/banner1.webp";
+import Navbar from "./Navbar";
 
 const Home = () => {
   return (
     <>
-      {/* <img src={banner}></img> */}
       <div
         className="h-screen w-full bg-cover bg-center bg-no-repeat bg-fixed"
         style={{ backgroundImage: `url(${banner})` }}
       >
-        <div className="flex items-center justify-center h-full bg-black/50">
-          <h1 className="text-white text-4xl font-bold">Welcome to My Site</h1>
+        <div className=" py-44 mx-10  h-full ">
+          <h1 className="text-white text-2xl font-semibold font-Vazir text-red py-6  ">
+            با تجهیزات حرفه‌ای کمپ، آماده‌ی کشف دنیای بیرون شو.
+          </h1>
+          <h1 className="text-white text-md font-semibold font-Vazir text-red ">
+            «آرامش واقعی اون‌جاست که به طبیعت پناه می‌بری.»
+          </h1>
         </div>
       </div>
     </>
