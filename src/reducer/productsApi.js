a49a9b01f5bd95770fef1d21ca2503cbe8baceb2
asma ezzati <asmaezzati2019@gmail.com>
2025-10-30 17:32:41 +0330

    create new js file for RTK Query

---
+++ src/reducer/productsApi.js
@@ -0,0 +1,13 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+
+export const productsApi = createApi({
+  reducerPath: "productsApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "htttp://localhost:9000/" }),
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: () => "products",
+    }),
+  }),
+});
+
+export const { useGetProductsQuery } = productsApi;
