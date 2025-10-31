a02bd801e42e90f1096247fd77023f4739a4309d
asma ezzati <asmaezzati2019@gmail.com>
2025-10-31 21:12:40 +0330

    find & fix issue

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
 import { productsApi } from "../reducer/productsApi";
 
 export const store = configureStore({
   reducer: {
     [productsApi.reducerPath]: productsApi.reducer,
   },
-
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(productsApi.middleware),
 });
