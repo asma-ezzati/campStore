c601e7e9179c8dc286b9b0925a703edeba295993
asma ezzati <asmaezzati2019@gmail.com>
2025-10-31 21:53:02 +0330

    Fetch data from server

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,25 +1,28 @@
 import { useGetProductsQuery } from "../reducer/productsApi";
 
 const Shop = () => {
-  //   const {
-  //     data: products,
-  //     isError,
-  //     isSuccess,
-  //     isLoading,
-  //   } = useGetProductsQuery();
-  // console.log(products);
+  const { data: products, isLoading, error } = useGetProductsQuery();
 
-  //   const limitedProducts = products.slice(0, 6);
-  //   console.log(limitedProducts);
+  if (isLoading) return <p>در حال بارگذاری...</p>;
+  if (error) return <p>خطایی رخ داده</p>;
 
-  // if (isError) return <p>Error</p>;
+  // فقط 6 تا محصول اول
+  const limitedProducts = products.slice(0, 6);
 
   return (
-    <>
-      <div className="h-screen m-2 rounded-md bg-brown flex justify-center items-center">
-        <div></div>
-      </div>
-    </>
+    <div className=" bg-brown m-2 rounded-md h-screen flex items-center justify-center flex-wrap  ">
+      {limitedProducts.map((product) => (
+        <div
+          key={product.id}
+          className=" border rounded-xl w-[300px] m-2 p-4 h-[250px] flex "
+        >
+          <img src={product.image} alt={product.title} className="w-1/2" />
+          <h2 className=" font-Vazir text-peach text-xl font-semibold">
+            {product.title}
+          </h2>
+        </div>
+      ))}
+    </div>
   );
 };
 export default Shop;
