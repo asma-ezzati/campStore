dafede3e5629c6685238ac0d30ab0f293e9f46bf
asma ezzati <asmaezzati2019@gmail.com>
2025-10-31 21:14:33 +0330

    test & find issues

--- src/reducer/productsApi.js
+++ src/reducer/productsApi.js
@@ -2,10 +2,10 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const productsApi = createApi({
   reducerPath: "productsApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "htttp://localhost:9000/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:9000/" }),
   endpoints: (builder) => ({
     getProducts: builder.query({
-      query: () => "products",
+      query: () => `products`,
     }),
   }),
 });
