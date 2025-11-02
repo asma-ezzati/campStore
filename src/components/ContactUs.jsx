1553e50646e3d5eb8a202ea86558acc2527a7b1a
asma ezzati <asmaezzati2019@gmail.com>
2025-11-02 13:13:57 +0330

    create new component for footer

---
+++ src/components/ContactUs.jsx
@@ -0,0 +1,10 @@
+const ContactUs = () => {
+  return (
+    <>
+      <div className="h-screen bg-red m-2 rounded-md">
+        <h1>contact us</h1>
+      </div>
+    </>
+  );
+};
+export default ContactUs;
