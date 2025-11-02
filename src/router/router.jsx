1bc634a58e28ec5c1396c70af4bea268d174c521
asma ezzati <asmaezzati2019@gmail.com>
2025-11-02 17:16:24 +0330

    add new route for productlist page

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import ProductsList from "../components/ProductsList";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/products",
+    element: <ProductsList />,
   },
 ]);
