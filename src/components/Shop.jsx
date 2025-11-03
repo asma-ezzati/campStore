ce5a0adf2550be4d64dc545e8f6b6bb4c99f1436
asma ezzati <asmaezzati2019@gmail.com>
2025-11-03 18:59:51 +0330

    update components & show categories as Link

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,35 +1,26 @@
 import { Link } from "react-router-dom";
-import { useGetProductsQuery } from "../reducer/productsApi";
+import { useGetCategoriesQuery } from "../reducer/productsApi";
 import { FiShoppingBag } from "react-icons/fi";
 
 const Shop = () => {
-  const { data: products, isLoading, error } = useGetProductsQuery();
+  const { data: category, error } = useGetCategoriesQuery();
 
-  if (isLoading) return <p>در حال بارگذاری...</p>;
-  if (error) return <p>خطایی رخ داده</p>;
-
-  // فقط 6 تا محصول اول
-  const limitedProducts = products.slice(0, 4);
+  if (!category) return [];
 
   return (
     <>
-      <div className=" bg-brown m-2 rounded-md h-screen flex items-center justify-center flex-wrap relative ">
-        {limitedProducts.map((product) => (
-          <div
-            key={product.id}
-            className=" rounded-tl-full rounded-tr-full w-[250px] m-2 p-4 h-[350px] bg-peach "
-          >
-            <img
-              src={product.image}
-              alt={product.title}
-              className="w-[200px]  m-2 mt-5"
-            />
-            <h2 className=" font-Vazir text-brown text-xl font-semibold mt-6 text-center">
-              {product.title}
-            </h2>
-          </div>
+      <div className=" bg-brown m-2 rounded-md h-screen grid grid-cols-5 grid-row-2 items-center  relative ">
+        {category.map((cat) => (
+          <Link key={cat.id}>
+            <div className=" mt-14 m-4 rounded-xl shadow-lg hover:shadow-bistre transition-shadow duration-300 h-[200px] bg-lion ">
+              <img src={cat.image} className="w-[150px] mx-auto p-2 "></img>
+              <h1 className=" font-Vazir text-xl text-peach text-center ">
+                {cat.name}
+              </h1>
+            </div>
+          </Link>
         ))}
-        <div className="absolute top-10 left-10 hover:transition-transform hover:-skew-x-12 hover:skew-y-3 ">
+        <div className="absolute bottom-10 left-10 hover:transition-transform hover:-skew-x-12 hover:skew-y-3  ">
           <Link
             to={"/products"}
             className=" font-Vazir text-xl text-peach flex "
