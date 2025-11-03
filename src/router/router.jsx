5b8d48e293dbb3e1710f737d3341f1f462fccb94
asma ezzati <asmaezzati2019@gmail.com>
2025-11-03 19:06:28 +0330

    create new route for category pages

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import ProductsList from "../components/ProductsList";
+import CategoryPage from "../components/CategoryPage";
 
 export const router = createBrowserRouter([
   {
@@ -10,5 +11,9 @@ export const router = createBrowserRouter([
   {
     path: "/products",
     element: <ProductsList />,
+  },
+  {
+    path: `/categories/:catId`,
+    element: <CategoryPage />,
   },
 ]);
