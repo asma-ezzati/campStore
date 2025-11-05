51a393540616b74da90aaa737e8586da7224af97
asma ezzati <asmaezzati2019@gmail.com>
2025-11-05 17:46:57 +0330

    add validation for formik in signup page

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,28 +1,60 @@
 import { ErrorMessage, Field, Form, Formik } from "formik";
 import banner3 from "../assets/banner3.jpg";
+import * as Yup from "yup";
 const Signin = () => {
+  const schema = Yup.object({
+    email: Yup.string()
+      .email("ایمیل معتبر نیست❌")
+      .required("ایمیل الزامی است 📍"),
+    password: Yup.string()
+      .max(8, "🚨 پسورد شامل 8 کاراکتر و کمتر باشد")
+      .required("پسورد الزامی است 📍"),
+  });
+
   return (
     <>
       <div className="h-screen bg-red grid place-items-center ">
         <div className="w-[60%] mx-auto  rounded-md grid grid-cols-3 grid-rows-1 bg-lion  ">
           <div className="col-span-2">
-            <h1 className="font-Vazir text-lg font-semibold p-2 mt-6 text-center text-red ">
+            <h1 className="font-Vazir text-lg font-semibold p-2 mt-6 text-center text-bistre ">
               ساخت حساب کاربری
             </h1>
 
             <Formik
               initialValues={{ email: "", password: "" }}
+              validationSchema={schema}
               onSubmit={() => {}}
             >
-              <Form>
-                <label>ایمیل</label>
-                <Field name="email" type="email" />
-                <ErrorMessage name="email" component="div"></ErrorMessage>
+              <Form className="font-Vazir p-5 ">
+                <Field
+                  name="email"
+                  type="email"
+                  placeholder="ایمیل"
+                  className="w-2/3 my-4 bg-lion border-2 border-bistre rounded-md p-2 focus:outline-none  block mx-auto placeholder:text-bistre focus:border-red "
+                />
+                <ErrorMessage
+                  name="email"
+                  component="div"
+                  className="font-Vazir text-md m-2 text-red  text-center"
+                ></ErrorMessage>
 
-                <label>پسورد</label>
-                <Field name="password" type="password" />
-                <ErrorMessage name="password" component="div"></ErrorMessage>
-                <button type="submit">ثبت نام</button>
+                <Field
+                  name="password"
+                  type="password"
+                  placeholder="پسورد"
+                  className="w-2/3 my-4 bg-lion border-2 border-bistre rounded-md p-2 focus:outline-none block mx-auto  placeholder:text-bistre focus:border-red"
+                />
+                <ErrorMessage
+                  name="password"
+                  component="div"
+                  className="font-Vazir text-md m-2 text-red  text-center"
+                ></ErrorMessage>
+                <button
+                  type="submit"
+                  className="w-2/3 my-4 mx-[77px] border-bistre border-2 p-2 rounded-md bg-bistre text-peach hover:shadow-xl shadow-bistre"
+                >
+                  ثبت نام
+                </button>
               </Form>
             </Formik>
           </div>
