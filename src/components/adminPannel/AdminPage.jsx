86c9d03ff10c98df95b5b2643a39421823d3fc17
asma ezzati <asmaezzati2019@gmail.com>
2025-11-06 11:48:30 +0330

    Add grid layout to page

--- src/components/adminPannel/AdminPage.jsx
+++ src/components/adminPannel/AdminPage.jsx
@@ -1,2 +1,16 @@
-const AdminPage = () => {};
+const AdminPage = () => {
+  return (
+    <>
+      <div className=" h-screen bg-lion  ">
+        <div className="font-Vazir text-center text-xl text-bistre font-bold py-6">
+          <h1>Admin Pannel</h1>
+        </div>
+        <div className="grid grid-cols-12">
+          <div className="sidebar border-2 h-min  "></div>
+          <div className="products"></div>
+        </div>
+      </div>
+    </>
+  );
+};
 export default AdminPage;
