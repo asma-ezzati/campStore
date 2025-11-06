7b19be5f56b4756d724206280685e6dab32cfae5
asma ezzati <asmaezzati2019@gmail.com>
2025-11-06 13:06:18 +0330

    add products in main admin page

--- src/components/adminPannel/AdminPage.jsx
+++ src/components/adminPannel/AdminPage.jsx
@@ -1,9 +1,10 @@
+import ProductsAdmin from "./ProductsAdmin";
 import Sidebar from "./Sidebar";
 
 const AdminPage = () => {
   return (
     <>
-      <div className=" h-screen bg-lion  ">
+      <div className=" bg-lion  ">
         <div className="font-Vazir text-center text-xl text-bistre font-bold py-6">
           <h1>Admin Pannel</h1>
         </div>
@@ -11,7 +12,9 @@ const AdminPage = () => {
           <div className=" ">
             <Sidebar />
           </div>
-          <div className="">{/* <Products/> */}</div>
+          <div className="col-span-11">
+            <ProductsAdmin />
+          </div>
         </div>
       </div>
     </>
