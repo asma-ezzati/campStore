1dead1243501538385b3728626d0d72d3169dfa3
asma ezzati <asmaezzati2019@gmail.com>
2025-11-06 12:49:38 +0330

    fetch data in srever

---
+++ src/components/adminPannel/ProductsAdmin.jsx
@@ -0,0 +1,69 @@
+import { useMemo } from "react";
+import {
+  useGetCategoriesQuery,
+  useGetProductsQuery,
+} from "../../reducer/productsApi";
+
+const ProductsAdmin = () => {
+  const { data: products } = useGetProductsQuery();
+  const { data: categories } = useGetCategoriesQuery();
+
+  const finalData = useMemo(() => {
+    if (!products || !categories) return [];
+    const catMap = new Map();
+    categories.forEach((cat) => {
+      catMap.set(String(cat.id), cat.name);
+    });
+    return products.map((p) => {
+      const catName = catMap.get(String(p.category)) ?? "نامشخص";
+      return { ...p, categoryName: catName };
+    });
+  }, [products, categories]);
+
+  return (
+    <>
+      <div className=" grid grid-cols-4  bg-peach bg-opacity-40  ">
+        <h1 className="col-span-4 font-Vazir text-bistre text-2xl font-bold text-center mt-6 ">
+          محصولات فروشگاه ما
+        </h1>
+        {finalData.map((product) => (
+          <div
+            key={product.id}
+            className=" border-2 border-lion rounded-md  m-2 w-[300px] h-[435px]  "
+          >
+            <img
+              src={product.image}
+              alt={product.title}
+              className="w-[200px] m-10  "
+            ></img>
+            <div className="grid grid-cols-4">
+              <div className="col-span-3">
+                <h1 className=" font-Vazir text-lg text-right text-bistre mr-3 ">
+                  نام محصول : {product.title}
+                </h1>
+                <h2 className=" font-Vazir text-lg text-right text-bistre mr-3 ">
+                  قیمت : {product.price}
+                </h2>
+                <h3 className=" font-Vazir text-lg text-right text-bistre mr-3 ">
+                  دسته بندی : {product.categoryName}
+                </h3>
+              </div>
+              <div className="grid grid-rows-1   ">
+                <Link
+                  to={`/products/${product.id}`}
+                  className="bg-bistre w-[55px] h-[55px] rounded-full m-2"
+                >
+                  <FaEye size={30} className="text-peach m-3 " />
+                </Link>
+                <button className="bg-bistre w-[55px] h-[55px] rounded-full m-2">
+                  <MdShoppingCart size={30} className="text-peach m-3 " />
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+export default ProductsAdmin;
