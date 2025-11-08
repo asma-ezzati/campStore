ed3cc9fbb5ffccd66e0927eac56273843ce3ecda
asma ezzati <asmaezzati2019@gmail.com>
2025-11-08 17:52:51 +0330

    delete product whith delete API & create event handler

--- src/components/adminPannel/DeleteProduct.jsx
+++ src/components/adminPannel/DeleteProduct.jsx
@@ -1,9 +1,25 @@
 import { MdDeleteForever } from "react-icons/md";
+import { useDeleteProductMutation } from "../../reducer/productsApi";
 
-const DeleteProduct = () => {
+const DeleteProduct = ({ id }) => {
+  const [deleteProduct] = useDeleteProductMutation();
+
+  const handleDleteButton = async () => {
+    try {
+      await deleteProduct(id).unwrap();
+      alert("محصول با موفقیت حذف شد ✅");
+    } catch (err) {
+      alert("خطا در حذف محصول❌");
+      console.log(err);
+    }
+  };
+
   return (
     <>
-      <button className="bg-bistre w-[55px] h-[55px] rounded-full m-2 hover:bg-red ">
+      <button
+        onClick={handleDleteButton}
+        className="bg-bistre w-[55px] h-[55px] rounded-full m-2 hover:bg-red "
+      >
         <MdDeleteForever size={30} className="text-peach m-3 " />
       </button>
     </>
