f96e728de24170873335aeef0d7f2c30dbccc5d8
asma ezzati <asmaezzati2019@gmail.com>
2025-11-08 18:57:23 +0330

    fix problem in Link (prop:to)

--- src/components/adminPannel/EditProducts.jsx
+++ src/components/adminPannel/EditProducts.jsx
@@ -1,2 +1,16 @@
-const EditProducts = () => {};
+import { FaUserEdit } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const EditProducts = ({ id }) => {
+  return (
+    <>
+      <Link
+        to={`/admin/edit/${id}`}
+        className="bg-bistre w-[55px] h-[55px] rounded-full m-2 hover:bg-brown "
+      >
+        <FaUserEdit size={30} className="text-peach m-3 " />
+      </Link>
+    </>
+  );
+};
 export default EditProducts;
