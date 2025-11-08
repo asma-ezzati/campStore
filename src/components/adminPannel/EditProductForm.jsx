7d14050e708c48283a66a65a290a5eaece0264a1
asma ezzati <asmaezzati2019@gmail.com>
2025-11-08 22:01:25 +0330

    take data as server

--- src/components/adminPannel/EditProductForm.jsx
+++ src/components/adminPannel/EditProductForm.jsx
@@ -1,4 +1,22 @@
+import { useParams } from "react-router-dom";
+import {
+  useEditProductsMutation,
+  useGetCategoriesQuery,
+  useGetProductByIdQuery,
+} from "../../reducer/productsApi";
+
 const EditProductsForm = () => {
-  return <h1>hello form</h1>;
+  const { productId } = useParams();
+
+  const { data: product, isLoading: productLoading } =
+    useGetProductByIdQuery(productId);
+  const [editProduct] = useEditProductsMutation();
+  const { data: categories } = useGetCategoriesQuery();
+
+  if (productLoading) return <div>loading....</div>;
+
+  if (!product) return <div>notfound product</div>;
+
+  return <></>;
 };
 export default EditProductsForm;
