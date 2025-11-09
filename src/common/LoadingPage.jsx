38467a5191c27bc6d144e8479abd6f66c9af0d89
asma ezzati <asmaezzati2019@gmail.com>
2025-11-09 22:28:10 +0330

    rename file Loading.jsx -> LoadingPage.jsx

--- src/common/LoadingPage.jsx
+++ src/common/LoadingPage.jsx
@@ -1,12 +1,15 @@
 import loading from "../assets/loading.svg";
 
-const Loading = () => {
+const LoadingPage = () => {
   return (
     <>
       <div className=" bg-lion flex justify-center items-center h-screen">
         <img src={loading} alt="loading" className="w-16 h-16"></img>
+        <h1 className="font-Vazir text-xl font-semibold text-bistre ">
+          در حال بارگذاری....
+        </h1>
       </div>
     </>
   );
 };
-export default Loading;
+export default LoadingPage;
