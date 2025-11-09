ee5eebb730da781939494bc46364c9acf1bff1d3
asma ezzati <asmaezzati2019@gmail.com>
2025-11-09 14:48:13 +0330

    turn icon to Link

--- src/components/adminPannel/Sidebar.jsx
+++ src/components/adminPannel/Sidebar.jsx
@@ -2,12 +2,15 @@ import { IoBagAddOutline, IoSettingsOutline } from "react-icons/io5";
 import { BsCalendar2Date } from "react-icons/bs";
 import { FaRegEye } from "react-icons/fa6";
 import { IoMdExit } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <>
       <div className="bg-[rgba(255,255,255,0.08)] rounded-lg w-[80px] m-1 p-1 ">
-        <IoBagAddOutline size={30} className="my-14 mx-5 text-bistre" />
+        <Link to={`/admin/add`}>
+          <IoBagAddOutline size={30} className="my-14 mx-5 text-bistre" />
+        </Link>
         <BsCalendar2Date size={30} className="my-14 mx-5 text-bistre" />
         <IoSettingsOutline size={30} className="my-14 mx-5 text-bistre" />
         <FaRegEye size={30} className="my-14 mx-5 text-bistre" />
