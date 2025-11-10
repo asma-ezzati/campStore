a5190d5a8677b33a36216d319346301fdcf80056
asma ezzati <asmaezzati2019@gmail.com>
2025-11-10 18:27:12 +0330

    check page & resize image

--- src/common/NotFoundPage.jsx
+++ src/common/NotFoundPage.jsx
@@ -3,7 +3,11 @@ const NotFoundPage = () => {
   return (
     <>
       <div className="h-screen flex justify-center items-center bg-lion ">
-        <img src={notFound} alt="notFound"></img>
+        <img
+          src={notFound}
+          alt="notFound"
+          className="w-[400px] h-[400px] "
+        ></img>
       </div>
     </>
   );
