1334b73c3e12a4763d6c63783bdaded44b30813b
asma ezzati <asmaezzati2019@gmail.com>
2025-11-23 19:34:58 +0330

    Responsive component for mobile & tablet

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -6,11 +6,11 @@ import { IoLogoWhatsapp } from "react-icons/io";
 const ContactUs = () => {
   return (
     <>
-      <div className="h-screen bg-red m-2 p-4 rounded-md grid grid-cols-3 gap-2  grid-rows-1 gap-y-0 items-center justify-center ">
-        <div className="border-l-2 border-peach">
+      <div className="min-h-screen bg-red m-2 p-4 rounded-md grid  lg:grid-cols-3 gap-2  grid-rows-1 gap-y-0 items-center  ">
+        <div className="lg:border-l-2 border-peach">
           <img
             src={banner2}
-            className="w-[250px] h-[200px] object-cover  mr-16 "
+            className="w-[250px] h-[200px] object-cover  mx-auto "
           ></img>
           <h1 className=" font-Vazir text-xl font-bold text-peach col-span-5 mt-10 text-center ">
             «فروشگاه وسایل کمپ »
@@ -19,7 +19,7 @@ const ContactUs = () => {
             تجربه طبیعت با تجهیزات حرفه‌ای
           </h1>
         </div>
-        <div className="border-l-2 border-peach ">
+        <div className="lg:border-l-2 border-peach ">
           <h2 className=" font-Vazir text-peach font-semibold text-lg p-6 ">
             شماره تماس :
           </h2>
@@ -37,7 +37,7 @@ const ContactUs = () => {
           <h1 className=" font-Vazir text-peach font-semibold text-lg p-4 text-center mb-2  ">
             شبکه های اجتماعی ما
           </h1>
-          <div className="grid grid-cols-5 grid-rows-1 items-center justify-center ">
+          <div className="grid grid-cols-5 grid-rows-1 items-center mr-7 sm:mr-16 pb-10">
             <FaTelegram size={30} className="text-peach" />
             <RiInstagramFill size={30} className="text-peach" />
             <IoLogoWhatsapp size={30} className="text-peach" />
